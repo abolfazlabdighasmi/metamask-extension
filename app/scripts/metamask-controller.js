96aaed115c907b3f5f3578c8203c604bc01b04ca
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2024-06-03 15:56:07 -0400

    adding a new property to state

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -3974,6 +3974,10 @@ export default class MetamaskController extends EventEmitter {
         this.metaMetricsDataDeletionController.updateDataDeletionTaskStatus.bind(
           this.metaMetricsDataDeletionController,
         ),
+      setHasMetaMetricsDataRecorded:
+        this.metaMetricsDataDeletionController.setHasMetaMetricsDataRecorded.bind(
+          this.metaMetricsDataDeletionController,
+        ),
     };
   }
 
