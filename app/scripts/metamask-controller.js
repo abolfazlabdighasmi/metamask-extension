8f96e970e7602ff6966bf8590e300738ec60a58c
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2024-07-10 16:48:53 -0400

    removed the setHasMetaMetricsDataRecorded instead update the variable while creating delete regulation

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -762,8 +762,9 @@ export default class MetamaskController extends EventEmitter {
         dataDeletionService,
         messenger: metaMetricsDataDeletionMessenger,
         state: initState.metaMetricsDataDeletionController,
-        getMetaMetricsId: () =>
-          this.metaMetricsController.store.getState().metaMetricsId,
+        getMetaMetricsId: () => this.metaMetricsController.state.metaMetricsId,
+        getParticipateInMetrics: () =>
+          this.metaMetricsController.state.participateInMetaMetrics,
       });
 
     const gasFeeMessenger = this.controllerMessenger.getRestricted({
@@ -3974,10 +3975,6 @@ export default class MetamaskController extends EventEmitter {
         ),
       updateDataDeletionTaskStatus:
         this.metaMetricsDataDeletionController.updateDataDeletionTaskStatus.bind(
-          this.metaMetricsDataDeletionController,
-        ),
-      setHasMetaMetricsDataRecorded:
-        this.metaMetricsDataDeletionController.setHasMetaMetricsDataRecorded.bind(
           this.metaMetricsDataDeletionController,
         ),
     };
